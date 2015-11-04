a9c2160f1fc1afbd8b3d5cd61bbb4c143f68b508
Marcelo Gutierrez <marceloaldo@gmail.com>
2015-11-04 09:33:58 -0300

    Initial version.

---
+++ jquery.jsonselect.js
@@ -0,0 +1,96 @@
+/**
+ * [description]
+ * <select name="my_select" id="my_select" class="any_decoration" data-empty-text="Select an option" data-url="some_json_resorce" data-parent="#some_parent_select">
+ * </select>
+ */
+(function ($) {
+
+var methods = {
+    _reset: function(select) {
+        var $self = select;
+        $self
+            .find('option')
+            .remove()
+            .end()
+            .append($('<option>').val('').text($self.data('jsonselect').empty_text));
+    },
+    load: function(select) {
+
+        var $self = select,
+            $parent = null,
+            params = $self.data('params');
+
+        $parent = $($self.data('parent'));
+
+        if ($parent.length) {
+            params = $.extend(true, {}, params, {id: $parent.val()});
+        }
+
+        $.ajax({
+            url: $self.data('url'),
+            dataType: 'json',
+            data: params,
+            success: function(data, textStatus, jqXHR) {
+                methods._reset($self);
+
+                var options = $(data).map(function () {
+                    var value = $self.data('value'),
+                        text = $self.data('text');
+
+                    $self.
+                        append(
+                            $('<option>').
+                            val(this[value]).
+                            text(this[text])
+                    );
+                });
+
+                $.isFunction($self.data('jsonselect').events.success ) && $self.data('jsonselect').events.success.call($self, [data, textStatus, jqXHR]);
+            }
+        });
+    }
+};
+// What does the jsonselect plugin do?
+$.fn.jsonselect = function(options) {
+
+    if (!this.length) {
+        return this;
+    }
+
+    this.each(function() {
+        var $this = $(this);
+
+        if (!$this.is('select')) {
+            $.error( 'Element ' +  $this.attr('name') + ' is not an select form control!' );
+            return this;
+        }
+
+        options = $.extend({}, $.fn.jsonselect.defaults, options);
+
+        $this.data('jsonselect', options);
+
+        if ($this.data('parent')) {
+            var parent = $($this.data('parent'));
+            parent.bind('change', function (event) {;
+                methods.load($this);
+            });
+        }
+
+        if ($this.data('autoload')) {
+            methods.load($this);
+        }
+    });
+
+    return this;
+};
+
+// default options
+$.fn.jsonselect.defaults = {
+    selected: null,
+    empty_text: 'Select an option',
+    events: {
+        success: null // function(data, textStatus, jqXHR){ return this; }
+    }
+};
+
+}) (jQuery);
